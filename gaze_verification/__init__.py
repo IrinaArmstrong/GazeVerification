acbf93dac8849948c5aa07aa23f1c3dcaf8e7fb2
Ирина Абдуллаева <a.irene.a@mail.ru>
2022-08-16 23:25:29 -0500

    package namespace added

---
+++ gaze_verification/__init__.py
@@ -0,0 +1,11 @@
+from data_utils.sample import Sample, Samples
+
+__all__ = [
+    # data utils
+    "Sample", "Samples",
+
+    # experiment setup utils
+    "logging_handler"
+]
+
+__package__ = "gaze_verification"
