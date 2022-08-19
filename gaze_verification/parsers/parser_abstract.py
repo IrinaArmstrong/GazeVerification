c728d3b3aed0665aba7d9810aecd7f6b9d6cfb38
Ирина Абдуллаева <a.irene.a@mail.ru>
2022-08-18 23:54:01 -0500

    added abs parser class for inheritance

--- gaze_verification/parsers/parser_abstract.py
+++ gaze_verification/parsers/parser_abstract.py
@@ -1,9 +1,11 @@
+import os
+from pathlib import Path
 from abc import ABC, abstractmethod
-
 from typing import Union, List
 
-from auto_ner.configurators.entity_configurator import EntityConfigurator
-from auto_ner.core.algorithm_abstract import AlgorithmAbstract
+from gaze_verification.parsers.parser_utils import is_dir_empty
+from gaze_verification.algorithm_abstract import AlgorithmAbstract
+from gaze_verification.data_utils.sample import Sample, Samples
 
 
 class ParserAbstract(AlgorithmAbstract, ABC):
@@ -11,16 +13,59 @@ class ParserAbstract(AlgorithmAbstract, ABC):
     Abstract class for reading input data in arbitrary formats into Instances.
     """
 
-    SEP_ENT = EntityConfigurator.SEP_ENT
-    SEP_ATTR = EntityConfigurator.SEP_ATTR
-    SEP_ATTRS = EntityConfigurator.SEP_ATTRS
+    def __init__(self, *args, data_column_names: Union[str, List[str]],
+                 target_column_name: str,
+                 **kwargs):
+        super().__init__(*args, **kwargs)
+        self._data_column_names = data_column_names
+        self._target_column_name = target_column_name
 
     @abstractmethod
-    def check_emptiness(self, data: Union[str, List[str]]):
+    def check_emptiness(self, data_path: Union[str, List[str], Path]):
         """
         Check if the input is non-empty.
 
-        :param data: path to the data file or folder.
-        :type data: Union[str, List[str]]
+        :param data_path: path to the data file or folder.
+        :type data_path: Union[str, List[str], Path]
         """
+        if os.path.isfile(data_path):
+            return
+        elif os.path.isdir(data_path):
+            if is_dir_empty(data_path):
+                raise ValueError(
+                    f"Class {self.__class__.__name__} ",
+                    f"did not find any files in {data_path} folder",
+                    f"can't process further. Please, check if input samples list is empty."
+                )
+            else:
+                return
+        else:
+            raise ValueError(
+                f"Class {self.__class__.__name__} "
+                f"did not find a file at {data_path} and it is not a folder; "
+                f"can't process further. Please, check if input samples list is empty."
+            )
+
+    def run(self, data_path: str, **kwargs) -> Samples:
+        """
+        Run parser for datasets for classification tasks.
+
+        :param data_path: Path to csv file
+        :type data_path: str
+
+        :return: Transformed input file/folder into Samples/Session(s)
+        :rtype: Samples
+        """
+        return self._process_data(data_path)
+
+    def _process_data(self, data_path: str) -> Samples:
+        raise NotImplementedError
+
+    def _make_sample(
+            self,
+            guid: str,
+            seq_id: int,
+            label: int,
+            session_id: int
+    ) -> Samples:
         raise NotImplementedError
